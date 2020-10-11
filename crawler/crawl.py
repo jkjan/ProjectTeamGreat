1b88c615180eabfa14323de58e609c76442e915d
201511091_EunhoKim <ster8@naver.com>
2020-10-11 10:17:40 +0900

    Revert "Revert "Merge pull request #5 from jkjan/main""
    This reverts commit a3d95c9254284e0063daca7e383d5a281a396a89.

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -1,28 +1,29 @@
 import sys
 import os
-from pathlib import Path
 from utils import *
 import pandas as pd
-import time
-import datetime
 
+
+# 데이터 열
 columns = ['년도', '가수', '제목', '남녀', '장르',
            '최고순위', '작사가', '작곡가', '소속사', '가사']
 
-init()
-
+# 인자 확인
 try:
     name = sys.argv[1]
 except IndexError:
     print("인자가 전달되지 않았습니다. 기본값(JKJ)으로 진행합니다.")
     name = "JKJ"
 
+# 경로 얻기
 path = os.getcwd() + "/" + name + "/"
 
+# 크롤링 할 아티스트 정보 가져오기
 artists = get_artists(path)
 recently_succeeded = 0
 total_info = []
 
+# 세이브 파일 확인
 try:
     current = open("crawler/recently_succeeded.txt", "rt")
     recently_succeeded = int(current.readline())
@@ -31,6 +32,9 @@ try:
     current.close()
 except FileNotFoundError:
     pass
+
+# 드라이버 초기화
+init()
 
 for i in range(recently_succeeded, len(artists)):
     songs_info = get_songs_info(artists[i])
@@ -54,14 +58,11 @@ for i in range(recently_succeeded, len(artists)):
         current = open("crawler/recently_succeeded.txt", "wt")
         current.write(str(i))
         current.close()
-
     except Exception as e:
         print(artists[i], "데이터를 저장하는 도중 예상치 못한 오류가 발생하였습니다.")
         print(e)
 
-    # next_start = datetime.datetime.now() + datetime.timedelta(minutes=15)
-    # print("15분간 휴식합니다.", next_start.strftime("%H시 %M분"), "에 재개합니다.")
-
+# 전체 파일 저장
 df = pd.DataFrame(total_info, columns=columns)
 df.to_csv(path + "songs_" + name + "_total.csv", index=False, encoding='utf8')
 print("전체 데이터를 저장하였습니다.")
